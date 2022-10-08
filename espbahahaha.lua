22b78c9c05f9b30fc93c05b03ecf60fd228b1d79
retraCcc <100065385+retraCcc@users.noreply.github.com>
2022-10-07 20:21:41 -0400

    Update espbahahaha.lua

--- espbahahaha.lua
+++ espbahahaha.lua
@@ -1391,7 +1391,7 @@ do
         local toggle__gradient = utility:Create("Image", {Vector2.new(0,0), toggle_frame}, {
             Size = utility:Size(1, 0, 1, 0, toggle_frame),
             Position = utility:Position(0, 0, 0 , 0, toggle_frame),
-            Transparency = 0.3,
+            Transparency = 0,
             Visible = page.open
         }, section.visibleContent)
         --
